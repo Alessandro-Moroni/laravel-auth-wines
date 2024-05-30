fa6d2ffba2e85478cc7293d6a0d82beeb947cddb
Alessandro-Moroni <alemoro98@gmail.com>
2024-05-30 10:40:36 +0200

    [add]Userseeder-Auth

---
+++ database/seeders/UserTableSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+class UserTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $user = new User();
+        $user->name = 'admin';
+        $user->email = 'admin@admin.com';
+        $user->password = Hash::make('ciao1234');
+        $user->save();
+    }
+}
